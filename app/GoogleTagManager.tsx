ebd7285debe42711aacd8eee11ea4392694a833a
Jonhy-Maker <peekskehelp@gmail.com>
2026-08-27 16:44:35 +0200

    Fix Google Tag Manager loading

--- app/GoogleTagManager.tsx
+++ app/GoogleTagManager.tsx
@@ -7,16 +7,8 @@ export default function GoogleTagManager() {
     <>
       <Script
         id="google-tag-manager"
+        src="https://www.googletagmanager.com/gtm.js?id=GTM-KG5S3LB7"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KG5S3LB7');
-          `,
-        }}
       />
 
       <noscript>
