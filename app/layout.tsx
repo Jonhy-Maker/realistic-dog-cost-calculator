c3c9fa5e46142268c7fbde95ec46d16f2ffad236
Jonhy-Maker <peekskehelp@gmail.com>
2026-08-27 15:29:44 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,6 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-MZKZHDE70L');
           `}
         </Script>
